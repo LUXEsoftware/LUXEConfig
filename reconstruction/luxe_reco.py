095f9b84c7dbc08b6d07420c39f1a6ce13b5fc13
melissa <melissa.almanza.soto@desy.de>
2026-02-17 17:18:37 +0100

    Added processors for silicon ecal digi and reco

--- reconstruction/luxe_reco.py
+++ reconstruction/luxe_reco.py
@@ -28,7 +28,6 @@ io_svc = IOSvc(
     "IOSvc",
     OutputLevel=INFO,
 )
-
 # Only load a geometry if it is passed
 if reco_args.compactFile:
     svc_list.append(
@@ -44,10 +43,38 @@ alg_list = []
 io_handler = IOHandlerHelper(alg_list, io_svc)
 io_handler.add_reader(reco_args.inputFile)
 
+MyEcalpReco = MarlinProcessorWrapper("MyEcalpReco")
+MyEcalpReco.ProcessorType = "RealisticCaloRecoSilicon"
+MyEcalpReco.Parameters = {
+    "CellIDLayerString": ["layer"],
+    "calibration_factorsMipGev": ["0.00616736103247","0.0125274552256"],
+    "calibration_layergroups": ["20", "1"],
+    "inputHitCollections": ["PixelSiEcalCollectionDigi"],
+    "inputRelationCollections": ["EcalpRelationsSimDigi"],
+    "outputHitCollections": ["PixelSiEcalCollectionRec"],
+    "outputRelationCollections": ["EcalpRelationsSimRec"],
+}
+
+MyEcalpDigi.ProcessorType = "RealisticCaloDigiSilicon"
+MyEcalpDigi.Parameters = {
+    "CellIDLayerString": ["layer"],
+    "calibration_mip": ["0.0001525"],
+    "inputHitCollections": ["PixelSiEcalCollection"],
+    "outputHitCollections": ["PixelSiEcalCollectionDigi"],
+    "outputRelationCollections": ["EcalpRelationsSimDigi"],
+    "threshold": ["0"],
+    "timingCut": ["1"],
+}
+
+alg_list.extend([
+    MyEcalpDigi,
+    MyEcalpReco    
+])
+
 
 io_handler.add_edm4hep_writer(reco_args.outputFileBase)
 io_handler.finalize_converters()
 
 app_mgr = ApplicationMgr(
-    TopAlg=alg_list, EvtSel="NONE", EvtMax=3, ExtSvc=svc_list, OutputLevel=INFO
+    TopAlg=alg_list, EvtSel="NONE", EvtMax=10, ExtSvc=svc_list, OutputLevel=INFO
 )
