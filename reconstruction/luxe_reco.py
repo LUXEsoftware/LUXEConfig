1534ee42ac3c25b47d42e3e9090c0642df48910f
Thomas Madlener <thomas.madlener@desy.de>
2026-02-17 17:22:32 +0100

    Add basic setup for running converters for wrapped processors

--- reconstruction/luxe_reco.py
+++ reconstruction/luxe_reco.py
@@ -4,7 +4,11 @@ from Configurables import GeoSvc, MarlinProcessorWrapper, EventDataSvc
 from Gaudi.Configuration import INFO
 from k4FWCore import ApplicationMgr, IOSvc
 from k4FWCore.parseArgs import parser
+from k4MarlinWrapper.io_helpers import IOHandlerHelper
 
+parser.add_argument(
+    "--inputFile", help="Input file from simulation", type=str, default=""
+)
 parser.add_argument(
     "-o",
     "--outputFile",
@@ -22,7 +26,6 @@ svc_list = [EventDataSvc("EventDataSvc")]
 # Setup inputs and outputs
 io_svc = IOSvc(
     "IOSvc",
-    Output=reco_args.outputFile,
     OutputLevel=INFO,
 )
 # Only load a geometry if it is passed
@@ -37,6 +40,8 @@ if reco_args.compactFile:
     )
 
 alg_list = []
+io_handler = IOHandlerHelper(alg_list, io_svc)
+io_handler.add_reader(reco_args.inputFile)
 
 MyEcalpReco = MarlinProcessorWrapper("MyEcalpReco")
 MyEcalpReco.ProcessorType = "RealisticCaloRecoSilicon"
@@ -66,6 +71,9 @@ alg_list.extend([
     MyEcalpReco    
 ])
 
+
+io_handler.add_edm4hep_writer(reco_args.outputFileBase)
+io_handler.finalize_converters()
 
 app_mgr = ApplicationMgr(
     TopAlg=alg_list, EvtSel="NONE", EvtMax=10, ExtSvc=svc_list, OutputLevel=INFO
